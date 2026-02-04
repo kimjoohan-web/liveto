2105b790a98690a37968304918121d32db8322c4
김주한 <joodding@naver.com>
2026-02-04 20:07:32 +0900

    docker 설치

--- config/settings/base.py
+++ config/settings/base.py
@@ -164,7 +164,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.environ.get('REDIS_HOST', '127.0.0.1'), 6379)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
