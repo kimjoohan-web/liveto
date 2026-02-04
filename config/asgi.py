10815f8c93d22551dc268a2400b767a31c19b70c
김주한 <joodding@naver.com>
2026-02-04 20:05:29 +0900

    docker 설치

--- config/asgi.py
+++ config/asgi.py
@@ -6,7 +6,7 @@ from django.core.asgi import get_asgi_application
 import chat.routing
 # from mysite import chat
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
