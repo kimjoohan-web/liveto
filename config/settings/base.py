acdd55e4d351a7572dedae694fd721a8ad3ba979
김주한 <joodding@naver.com>
2026-02-04 18:06:12 +0900

    static 파일 업로드

--- config/settings/base.py
+++ config/settings/base.py
@@ -164,7 +164,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.environ.get('REDIS_HOST', '127.0.0.1'), 6379)],
+            "hosts": [(os.environ.get('REDIS_HOST', '43.201.107.113'), 6379)],
         },
     },
 }
