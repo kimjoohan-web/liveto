a2b4e811a40a73e7c55b7807d6c1ebdea23e82a2
김주한 <joodding@naver.com>
2026-02-06 16:02:36 +0900

    설정

--- config/asgi.py
+++ config/asgi.py
@@ -1,18 +1,42 @@
-import os
+# import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import chat.routing
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+# import chat.routing
+
 # from mysite import chat
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
 
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+# application = ProtocolTypeRouter({
+#   "http": get_asgi_application(),
+#   "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         )
+#     ),
+# }) 
+
+
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator 
+from chat.routing import websocket_urlpatterns
+from django.core.asgi import get_asgi_application
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
+django_asgi_app = get_asgi_application()
+# 클라이언트와 Channels 개발 서버가 연결 될 때, 어느 protocol 타입의 연결인지
+
+application = ProtocolTypeRouter({ 
+    'http': django_asgi_app,
+    # (http->django views is added by default)
+    # 만약에 websocket protocol 이라면, AuthMiddlewareStack
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        # URLRouter 로 연결, 소비자의 라우트 연결 HTTP path를 조사
     ),
 })
