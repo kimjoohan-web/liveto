bed058f7584e8d3bd088e410430e32f65d277b21
김주한 <joodding@naver.com>
2026-02-06 16:40:59 +0900

    설정

--- config/asgi.py
+++ config/asgi.py
@@ -1,9 +1,9 @@
 import os
-
+import chat.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.routing
+
 # from mysite import chat
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
