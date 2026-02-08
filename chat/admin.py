c0c5b517331bf2992225f4c34b4fbe0dd30c5fba
김주한 <joodding@naver.com>
2026-02-09 00:31:01 +0900

    admin 필드 수정

--- chat/admin.py
+++ chat/admin.py
@@ -15,3 +15,6 @@ class ChatRoomAdmin(admin.ModelAdmin):
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('chat_room', 'sender', 'content', 'timestamp')  # 관리자 페이지에 표시할 필드
     search_fields   = ('sender', 'content')  # 검색 필드 설정   
+
+admin.site.register(ChatRoom, ChatRoomAdmin)
+admin.site.register(Message, MessageAdmin)  
