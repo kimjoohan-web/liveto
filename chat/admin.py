93026a21f832ff39affd04f6a53ff4e81e168e6c
김주한 <joodding@naver.com>
2026-02-09 00:35:56 +0900

    admin 등록 안함

--- chat/admin.py
+++ chat/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-from django.utils.html import format_html
-# Register your models here.
-from .models import ChatRoom, Message # 모델 가져오기
-admin.site.register(ChatRoom,Message)
-
 
 
 
