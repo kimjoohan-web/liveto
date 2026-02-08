9fe8821ec358baf59548921fa6af2b6b7dd3c614
김주한 <joodding@naver.com>
2026-02-09 00:28:17 +0900

    admin 필드 수정

--- chat/admin.py
+++ chat/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 
 # Register your models here.
 from .models import ChatRoom, Message # 모델 가져오기
 
 admin.site.register(ChatRoom)
 admin.site.register(Message)
+
+class ChatRoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')  # 관리자 페이지에 표시할 필드
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('chat_room', 'sender', 'content', 'timestamp')  # 관리자 페이지에 표시할 필드
+    search_fields   = ('sender', 'content')  # 검색 필드 설정   
