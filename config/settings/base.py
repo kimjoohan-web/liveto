85b35c23664f295e03ff10cda30b19e127a9118c
김주한 <joodding@naver.com>
2026-05-21 15:46:59 +0900

    mssql연결 문자 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -118,6 +118,8 @@ DATABASES = {
         'PORT': '1433',                  # MSSQL 기본 포트
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server', # 설치된 ODBC 버전에 맞게 수정 (예: 18 등)
+            'trust_server_certificate': 'yes',  # SSL 인증서 검증 비활성화 (개발 환경에서만 사용)
+            'encrypt': 'no',  # SSL 암호화 비활성화 (개발 환경에서만 사용)
         },
     },
 
