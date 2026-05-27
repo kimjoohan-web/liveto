4edae2489293c2a020b00eda77812cb9b0def7b6
김주한 <joodding@naver.com>
2026-05-27 16:28:28 +0900

    mssql 연결 스트링

--- config/settings/base.py
+++ config/settings/base.py
@@ -117,9 +117,8 @@ DATABASES = {
         'HOST': 'wws12-002.cafe24.com',    # 로컬인 경우 'localhost' 또는 '127.0.0.1'
         'PORT': '1433',                  # MSSQL 기본 포트
         'OPTIONS': {
-            'driver': 'ODBC Driver 17 for SQL Server', # 설치된 ODBC 버전에 맞게 수정 (예: 18 등)
-            'trust_server_certificate': 'yes',  # SSL 인증서 검증 비활성화 (개발 환경에서만 사용)
-            'encrypt': 'yes',  # SSL 암호화 비활성화 (개발 환경에서만 사용)
+            'driver': 'ODBC Driver 17 for SQL Server', # 설치된 ODBC 버전에 맞게 수정 (예: 18 등)            
+            'encrypt': 'no',  # SSL 암호화 비활성화 (개발 환경에서만 사용)
         },
     },
 
