a27ec90dee1538676dcb195c812d8f2c6ac01dfd
김주한 <joodding@naver.com>
2026-06-02 10:09:33 +0900

    postgreSql 연결설정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,9 +1,12 @@
 
+import environ
 from .base import *
+
 # ALLOWED_HOSTS = ['43.201.107.113','127.0.0.1','localhost']
 ALLOWED_HOSTS = ['*']
 
-
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 # STATIC_ROOT = BASE_DIR / 'static/'
 
 
@@ -27,3 +30,15 @@ STATICFILES_DIRS = [
 
 # STATIC_ROOT = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'/static/')
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}
