e88366ab7661b82def3e544aac26e37721696904
김주한 <joodding@naver.com>
2026-06-05 13:27:54 +0900

    setting 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import environ
 from pathlib import Path
 import os
 
@@ -23,6 +23,11 @@ from django.views.static import serve
         
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -104,28 +109,39 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     },
+    
+#     'mssql_db': {
+#         'ENGINE': 'sql_server.pyodbc',
+#         'NAME': 'nicejoodding',       # MSSQL DB 이름
+#         'USER': 'nicejoodding',             # 예: sa
+#         'PASSWORD': 'fdkwon8824@',            # DB 접속 패스워드
+#         'HOST': 'wws12-002.cafe24.com',    # 로컬인 경우 'localhost' 또는 '127.0.0.1'
+#         'PORT': '1433',                  # MSSQL 기본 포트
+#         'OPTIONS': {
+#             'driver': 'ODBC Driver 17 for SQL Server', # 설치된 ODBC 버전에 맞게 수정 (예: 18 등)            
+#             'encrypt': 'no',  # SSL 암호화 비활성화 (개발 환경에서만 사용)
+#         },
+#     },
+
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    
-    'mssql_db': {
-        'ENGINE': 'sql_server.pyodbc',
-        'NAME': 'nicejoodding',       # MSSQL DB 이름
-        'USER': 'nicejoodding',             # 예: sa
-        'PASSWORD': 'fdkwon8824@',            # DB 접속 패스워드
-        'HOST': 'wws12-002.cafe24.com',    # 로컬인 경우 'localhost' 또는 '127.0.0.1'
-        'PORT': '1433',                  # MSSQL 기본 포트
-        'OPTIONS': {
-            'driver': 'ODBC Driver 17 for SQL Server', # 설치된 ODBC 버전에 맞게 수정 (예: 18 등)            
-            'encrypt': 'no',  # SSL 암호화 비활성화 (개발 환경에서만 사용)
-        },
-    },
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
