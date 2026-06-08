d5198f7da88651e05f401c4ddbc98a5df670acc6
김주한 <joodding@naver.com>
2026-06-08 15:11:29 +0900

    static

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -27,7 +27,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '../static/'
 STATICFILES_DIRS = [
    BASE_DIR /'static/',  
    
