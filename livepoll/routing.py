faae13b79ce53137bfeb4d717c50a4899359d092
kimjoohan <joodding@naver.com>
2026-07-31 22:10:18 +0900

    wss 경로수정

--- livepoll/routing.py
+++ livepoll/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/live_poll_(?P<int:stream_id>\d+)/$', consumers.PollConsumer.as_asgi()),
+    re_path(r'ws/live_poll_(?P<stream_id>\d+)/$', consumers.PollConsumer.as_asgi()),
 ]
