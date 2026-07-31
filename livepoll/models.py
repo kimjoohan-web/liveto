30d11bed173408ab60e8c374fc5761179a225f40
kimjoohan <joodding@naver.com>
2026-07-31 23:16:35 +0900

    wss 경로수정

--- livepoll/models.py
+++ livepoll/models.py
@@ -50,7 +50,8 @@ class Answer(models.Model):
     # user 필드는 member.event_member 테이블과 연결되어야 합니다.
 
     user = models.ForeignKey(
-        'member.event_member',  # event_member 모델의 mem_idx 필드와 연결
+        user_id='mem_idx',  # member.event_member 테이블의 기본 키를 참조
+        to='member.event_member',  # 참조할 모델 지정
         on_delete=models.SET_NULL, 
         null=True, 
         blank=True
