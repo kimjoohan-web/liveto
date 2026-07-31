f5d4b10f81006bd86303445669bec8bdb3c9ad5a
kimjoohan <joodding@naver.com>
2026-07-31 23:06:13 +0900

    wss 경로수정

--- livepoll/models.py
+++ livepoll/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.conf import settings
 
-import member.models
+# import 윗단계
+# 절대경로 
+
 
 
 # Create your models here.
@@ -48,7 +50,7 @@ class Answer(models.Model):
     # user 필드는 member.event_member 테이블과 연결되어야 합니다.
 
     user = models.ForeignKey(
-        member.models.event_member.mem_idx.field.related_model,  # event_member 모델의 mem_idx 필드와 연결
+        'member.event_member.mem_idx',  # event_member 모델의 mem_idx 필드와 연결
         on_delete=models.SET_NULL, 
         null=True, 
         blank=True
